3af94cc60084da8a579ee47a41f9ba1818e2127d
Sylvain Boulade <sylvain@occitech.fr>
2018-04-04 15:54:41 +0200

    Add util for resolving FC media - refs #11778

--- src/web/theme/modules/ProductItem/ProductItem.js
+++ src/web/theme/modules/ProductItem/ProductItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Price from "../../ui/atoms/Typography/Price";
 import Sku from "../../ui/atoms/Typography/Sku";
+import createMediaUrlFromPath from "../../../utils/createMediaUrlFromPath";
 
-const ProductItem = ({ path, name, prices, sku }) => {
+const ProductItem = ({ path, name, prices, sku, imageUrl }) => {
   return (
     <div className="product-item">
+      <img src={createMediaUrlFromPath(imageUrl)} alt={name} />
       path: {path}
       name: {name}
       <Price price={prices.finalPrice.priceInclTax} />
