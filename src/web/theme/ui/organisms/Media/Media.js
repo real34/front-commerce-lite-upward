b4c813fe24a23c2e5ed3835417a6a68457e6edd8
Julien Pradet <julien@occitech.fr>
2018-04-09 09:55:23 +0200

    Remove meaningless complexity - refs #11779
    Previsouly, the details weren't always rendered.
    In order to keep the animation as simple as possible, it is now always
    rendered. Hence we don't need javascript events.

--- src/web/theme/ui/organisms/Media/Media.js
+++ src/web/theme/ui/organisms/Media/Media.js
@@ -3,53 +3,13 @@ import PropTypes from "prop-types";
 import "./Media.scss";
 
 class Media extends Component {
-  constructor() {
-    super();
-    this.state = {
-      renderDetails: false
-    };
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseLeave = this.handleMouseLeave.bind(this);
-  }
-
-  handleMouseEnter(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    this.setState({
-      renderDetails: true
-    });
-  }
-
-  handleMouseLeave(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    this.setState({
-      renderDetails: false
-    });
-  }
-
   render() {
     const { media, renderDetails } = this.props;
 
     return (
-      <div
-        className="media"
-        tabIndex="0"
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        onFocus={this.handleMouseEnter}
-        onBlur={this.handleMouseLeave}
-      >
+      <div className="media" tabIndex="0">
         {media}
-        <div
-          className={`media__details ${
-            this.state.renderDetails ? "media__details--opened" : ""
-          }`}
-        >
-          {renderDetails()}
-        </div>
+        <div className="media__details">{renderDetails()}</div>
       </div>
     );
   }
