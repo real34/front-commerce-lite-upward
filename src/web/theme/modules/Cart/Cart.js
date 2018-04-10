cddf7541403b45a9175b9b401f8c48382a1022d4
Julien Pradet <julien@occitech.fr>
2018-04-10 15:42:14 +0200

    Add no checkout notice - refs #11779

--- src/web/theme/modules/Cart/Cart.js
+++ src/web/theme/modules/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import EnhanceCart from "./EnhanceCart";
 import CartQuery from "./CartQuery.gql";
 import CartItem from "./Item";
@@ -11,54 +11,101 @@ import Button from "../../ui/atoms/Button";
 import LoadingArea from "../../ui/molecules/LoadingArea";
 import Recap from "../../ui/organisms/Recap";
 import RecapTotal from "../../ui/organisms/RecapTotal";
+import Link from "../../ui/atoms/Typography/Link";
 
-const Cart = ({ cart, loading }) => {
-  if (loading) {
-    return <LoadingArea>Loading…</LoadingArea>;
+class Cart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      goToCheckout: false
+    };
+    this.launchCheckout = this.launchCheckout.bind(this);
   }
 
-  const cartLength =
-    cart && cart.items && cart.items.length ? cart.items.length : 0;
+  launchCheckout() {
+    this.setState({
+      goToCheckout: true
+    });
+  }
 
-  return (
-    <Fragment>
-      <ModalHeader>
-        My Cart
-        {cartLength > 0
-          ? cartLength === 1 ? " - 1 item" : ` - ${cart.items.length} items`
-          : null}
-      </ModalHeader>
-      <ModalContent>
-        {cartLength > 0 ? (
-          <Recap
-            total={
-              <RecapTotal
-                title="Subtotal"
-                price={cart.totals.subtotalInclTax}
-              />
-            }
-          >
-            {cart.items.map(item => (
-              <CartItem
-                key={item.item_id}
-                price={item.priceInfo.rowTotalInclTax}
-                imageUrl={item.product.imageUrl}
-                name={item.name}
-                sku={item.sku}
-                qty={item.qty}
-                id={item.item_id}
-              />
-            ))}
-          </Recap>
-        ) : (
-          <div>Your cart is empty.</div>
-        )}
-      </ModalContent>
-      <ModalAction>
-        <Button>Proceed to checkout</Button>
-      </ModalAction>
-    </Fragment>
-  );
-};
+  render() {
+    const { cart, loading } = this.props;
+    if (loading) {
+      return (
+        <Fragment>
+          <ModalHeader>My Cart</ModalHeader>
+          <ModalContent>
+            <LoadingArea>Loading…</LoadingArea>
+          </ModalContent>
+        </Fragment>
+      );
+    }
+
+    const cartLength =
+      cart && cart.items && cart.items.length ? cart.items.length : 0;
+
+    let content = null;
+    if (this.state.goToCheckout) {
+      content = (
+        <ModalContent>
+          There is no Checkout in Front Commerce Lite.<br />
+          However, feel free to take a look at{" "}
+          <Link to="https://demo.front-commerce.com/" external>
+            Front-Commerce's demo
+          </Link>{" "}
+          to see how it could look like.
+        </ModalContent>
+      );
+    } else {
+      content = (
+        <Fragment>
+          <ModalContent>
+            {cartLength > 0 ? (
+              <Recap
+                total={
+                  <RecapTotal
+                    title="Subtotal"
+                    price={cart.totals.subtotalInclTax}
+                  />
+                }
+              >
+                {cart.items.map(item => (
+                  <CartItem
+                    key={item.item_id}
+                    price={item.priceInfo.rowTotalInclTax}
+                    imageUrl={item.product.imageUrl}
+                    name={item.name}
+                    sku={item.sku}
+                    qty={item.qty}
+                    id={item.item_id}
+                  />
+                ))}
+              </Recap>
+            ) : (
+              <div>Your cart is empty.</div>
+            )}
+          </ModalContent>
+          <ModalAction>
+            <Button onClick={this.launchCheckout} type="full-size">
+              Proceed to checkout
+            </Button>
+          </ModalAction>
+        </Fragment>
+      );
+    }
+
+    return (
+      <Fragment>
+        <ModalHeader>
+          My Cart
+          {cartLength > 0
+            ? cartLength === 1 ? " - 1 item" : ` - ${cart.items.length} items`
+            : null}
+        </ModalHeader>
+        {content}
+      </Fragment>
+    );
+  }
+}
 
 export default EnhanceCart(CartQuery)(Cart);
