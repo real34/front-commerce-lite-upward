7d99b594c528cdac0d93a755d6d7ead917c9333b
Adrien Louis-Rossignol <adrien@occitech.fr>
2018-05-04 10:04:45 +0200

    Add @babel/preset-stage-0

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ const universalRules = [
     use: {
       loader: "babel-loader",
       options: {
-        presets: ["@babel/preset-env", "@babel/react"]
+        presets: ["@babel/preset-env", "@babel/react", "@babel/preset-stage-0"]
       }
     }
   },
