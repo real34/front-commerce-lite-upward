bc76e939e3755a6fb4a0686e400cef1e5d25b06e
Adrien Louis-Rossignol <adrien@occitech.fr>
2018-06-28 14:24:14 +0200

    Add a really basic production build setup

---
+++ webpack.config.prod.js
@@ -0,0 +1,128 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const NodeExternals = require("webpack-node-externals");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const sass = {
+  globalsImportStatements: '@import "globals.scss";',
+  globalsImportPaths: [path.resolve(__dirname, "src/web/theme")]
+};
+
+const srcPath = path.resolve(__dirname, "src");
+
+const universalRules = [
+  {
+    test: /\.js$/,
+    include: [srcPath],
+    loader: "eslint-loader",
+    enforce: "pre"
+  },
+  {
+    test: /\.js$/,
+    include: [srcPath],
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-env",
+          "@babel/react",
+          [
+            "@babel/preset-stage-0",
+            {
+              decoratorsLegacy: true
+            }
+          ]
+        ]
+      }
+    }
+  },
+  {
+    test: /\.(graphql|gql)$/,
+    include: [srcPath],
+    loader: "graphql-tag/loader"
+  },
+  {
+    test: /\.scss$/,
+    include: [srcPath],
+    use: [
+      {
+        loader: "style-loader"
+      },
+      {
+        loader: "css-loader"
+      },
+      {
+        loader: "sass-loader",
+        options: {
+          data: sass.globalsImportStatements,
+          includePaths: sass.globalsImportPaths
+        }
+      }
+    ]
+  },
+  {
+    test: /\.svg$/,
+    oneOf: [
+      {
+        loader: "raw-loader",
+        resourceQuery: /inline/
+      },
+      {
+        loader: "file-loader",
+        query: {
+          name: "static/media/[name].[hash:8].[ext]"
+        }
+      }
+    ]
+  },
+  {
+    test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+    loader: "file-loader",
+    query: {
+      name: "static/media/[name].[hash:8].[ext]"
+    }
+  }
+];
+
+module.exports = [
+  {
+    name: "client",
+    mode: "production",
+    entry: path.resolve(__dirname, "src/web/index.js"),
+    output: {
+      filename: "bundle.js",
+      publicPath: "/",
+      path: path.resolve(__dirname, "build/client")
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html"
+      }),
+      new webpack.NamedModulesPlugin()
+    ],
+    module: {
+      rules: [...universalRules]
+    },
+    resolve: {
+      alias: {
+        theme: path.resolve(__dirname, "src/web/theme")
+      }
+    }
+  },
+  {
+    name: "server",
+    mode: "production",
+    target: "node",
+    externals: [NodeExternals()],
+    entry: ["babel-polyfill", path.resolve(__dirname, "src/server/index.js")],
+    output: {
+      filename: "server.js",
+      path: path.resolve(__dirname, "build/server")
+    },
+    module: {
+      rules: [...universalRules]
+    }
+  }
+];
