c1c771a100042639c92f31b258dc4b51c3805fc5
emile <emile31.roch@gmail.com>
2018-06-29 10:34:29 +0200

    Create aa loading spinner

---
+++ src/web/theme/ui/atoms/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <span className="loading">
+      <span className="loading__cube1" />
+      <span className="loading__cube2" />
+    </span>
+  );
+};
+
+export default Loading;
