0ded09bb1894d308c25fd97ade8e760adbb107fb
Pierre Martin <pierre@occitech.fr>
2018-10-06 17:50:41 +0200

    UPWARD now proxies the demo Front-Commerce GraphQL endpoint
    - for now the frontend has been adapted to not use custom module types
    - there is still an issue to propagate cookies when proxying

--- src/web/theme/modules/Footer/Footer.js
+++ src/web/theme/modules/Footer/Footer.js
@@ -1,25 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types";
-import StoreDetails from "../StoreDetails";
-import StoreContact from "../StoreContact";
-import EnhanceFooter from "./EnhanceFooter";
-import FooterQuery from "./FooterQuery.gql";
+// import PropTypes from "prop-types";
+// import StoreDetails from "../StoreDetails";
+// import StoreContact from "../StoreContact";
+// import EnhanceFooter from "./EnhanceFooter";
+// import FooterQuery from "./FooterQuery.gql";
+import Link from "theme/ui/atoms/Typography/Link";
 import "./Footer.scss";
 
-const Footer = ({ store }) => {
+const Footer = () => {
   return (
     <footer className="footer">
-      {store && store.owner && <StoreDetails owner={store.owner} />}
-      {store && store.phone && <StoreContact phone={store.phone} />}
+      {/*store && store.owner && <StoreDetails owner={store.owner} />}
+      {store && store.phone && <StoreContact phone={store.phone} />*/}
+      Made with ❤️ by the{" "}
+      <Link to="https://www.front-commerce.com">Front-Commerce team</Link>
     </footer>
   );
 };
 
 Footer.propTypes = {
-  store: PropTypes.shape({
-    owner: StoreDetails.propTypes.owner,
-    phone: StoreContact.propTypes.phone
-  })
+  // store: PropTypes.shape({
+  //   owner: StoreDetails.propTypes.owner,
+  //   phone: StoreContact.propTypes.phone
+  // })
 };
 
-export default EnhanceFooter(FooterQuery)(Footer);
+// export default EnhanceFooter(FooterQuery)(Footer);
+export default Footer;
